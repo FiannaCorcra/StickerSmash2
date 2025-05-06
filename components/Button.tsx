d838dcbc5d4abc5ae320f6e6bd6ee9ef90515def
FiannaCorcra <24aw0109@jec.ac.jp>
2025-05-06 22:47:58 +0900

    Create a modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -25,8 +25,8 @@ export default function Button({ label, theme, onPress }: Props) {
 
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
-        <Text style={styles.buttonLabel}>{label}</Text>
+          <Pressable style={styles.button} onPress={onPress}>
+          <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
   );
